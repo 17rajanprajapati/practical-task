cc0e130b559794ee4d848dff85169529b4e4ffd1
PrateekJain9997 <prateek.jain@chicmic.co.in>
2022-02-17 19:10:59 +0530

    -- model changes

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,15 +1,22 @@
 /************* Modules ***********/
 const MONGOOSE = require('mongoose');
 const Schema = MONGOOSE.Schema;
+
+const { GENDER, LANGUAGE } = require('../utils/constants');
 /**************************************************
  ************* User Model or collection ***********
  **************************************************/
 const userSchema = new Schema({
-    firstName: { type: String },
-    lastName: { type: String },
+    fullName: { type: String },
     email: { type: String, required: true },
     password: { type: String },
-    profileImage: { type: String }
+    gender: { type: String, enum: [...Object.values(GENDER)] },
+    phoneNumber: { type: String },
+    language: { type: String, enum: [...Object.values(LANGUAGE)] },
+    martialStatus: { type: Boolean, default: false },
+    dob: { type: Date },
+    timeOfBirth: { type: String },
+    profileImageUrl: { type: String }
 });
 
 userSchema.set('timestamps', true);
